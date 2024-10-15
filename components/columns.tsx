49c53d3d03332ae0165f8dd8b7f791262a07a4aa
sam new lap <sam@gmail.com>
2024-10-15 22:11:11 +0530

    store presist to local storage

--- components/columns.tsx
+++ components/columns.tsx
@@ -1,10 +1,14 @@
+import NewTodoDialog from "./add-new-dialog";
 import Column from "./column";
 export default function Columns() {
   return (
-    <section className="flex h-full gap-6 lg:gap-12">
-      <Column status="To Do" />
-      <Column status="In Progress" />
-      <Column status="Done" />
-    </section>
+    <div>
+      <NewTodoDialog />
+      <section className="mt-10 flex gap-6 lg:gap-12">
+        <Column title="To Do" status="TODO" />
+        <Column title="In Progress" status="IN_PROGRESS" />
+        <Column title="Done" status="DONE" />
+      </section>
+    </div>
   );
 }
